1cede0e146c8da007ec7431653af5226df566dd3
GrYours <786646652@qq.com>
2017-05-28 00:55:26 -0700

    version 1.0

--- gem5py_main.py
+++ gem5py_main.py
@@ -2,9 +2,12 @@
 import os
 import sys, getopt
 import re
+#import numpy as np    #等矩阵处理的时候再考虑
+import plot_line
 
 inputfolder = []
-bench_name = []
+#bench_name = []
+sim_dict = {}  #通过dict进行记录，方便查询
 
 def find_dir(argv):
     try:
@@ -12,7 +15,9 @@ def find_dir(argv):
     except getopt.GetoptError:
         print 'python main.py -d <folder> -d <folder> ...'
         sys.exit(2)
+
     for opt, arg in opts:
+        #根据需要可以再添加参数！
         if opt == '-h':
             print 'python main.py -d <folder> -d <folder> ...'
             print 'such as l1size16kB'
@@ -20,29 +25,52 @@ def find_dir(argv):
             sys.exit()
         elif opt == '-d':
             if arg == '.':
-                cur_path = os.getcwd()     #当前目录
-                get_dir = os.listdir(cur_path)  #遍历当前目录，获取文件列表
+                cur_path = os.getcwd()           #当前目录
+                get_dir = os.listdir(cur_path)   #遍历当前目录，获取文件列表
                 for sub_dir in get_dir:
-                    if os.path.isdir(sub_dir):     #如果当前是文件夹
-                        inputfolder.append(sub_dir)
+                    if os.path.isdir(sub_dir):   #如果当前是文件夹
+                        if sub_dir != '.git':    #其他类型的文件夹这里没有进行过滤处理
+                            inputfolder.append(sub_dir)
             else:
                 inputfolder.append(arg)
+
     print '文件夹为：', inputfolder
 
 def main(argv):
     find_dir(argv)
-    for dirs in inputfolder:
-        path = os.path.join('.', dirs)
+
+    for sim_name in inputfolder:
+        bench_index_temp = {} 
+        path = os.path.join('.', sim_name)
         for files in os.listdir(path): 
-            if os.path.splitext(files)[1] == '.stats':  #只打开.stats格式的文件
-                bench_name.append(os.path.splitext(files)[0]) #bench name
-                files = os.path.join(dirs, files)       #要加上路径
+            if os.path.splitext(files)[1] == '.stats':        #只打开.stats格式的文件
+                bench_name = os.path.splitext(files)[0]       #bench name
+                files = os.path.join(sim_name, files)         #要加上路径
                 f = open(files, 'r')
-
-                guid = f.readlines()[2]
-                print guid
-
+                index_temp = {}
+                for context in f.readlines()[2:1037]:
+                    #print contexts
+                    item = re.findall(r'(\S+)\s+(\d+[.]{0,1}\d+)\s+(#\s.+)\n', context)
+                    #print item
+                    if item == []:
+                        #print 'None!Waiting me to deal with it!'
+                        no = 1
+                    else:
+                        index_temp[item[0][0]] = float(item[0][1])   #注释没有建立dict，这个之后再说
+                    
+                #print index_temp
+                bench_index_temp[bench_name] = index_temp
                 f.close()
+        #print bench_index_temp
+        sim_dict[sim_name] = bench_index_temp
+    #do a test!
+    #sim_name0   = 'l2size64kB'
+    #bench_name0 = 'bcnt'
+    valuename  = 'system.cpu.dcache.overall_miss_rate::total'
+    plot_line.plot_2D_single(sim_dict, valuename)
+    # for i0 in sim_dict:
+    #     for i1 in sim_dict[i0]:
+    #         print i1
             
 if __name__ == "__main__": 
     main(sys.argv[1:])
--- plot_line.py
+++ plot_line.py
@@ -1,17 +1,42 @@
+#-*- coding:utf-8 -*-
 from bokeh.plotting import figure, output_file, show
 
-# prepare some data
-x = [1, 2, 3, 4, 5]
-y = [6, 7, 2, 4, 5]
+color_index = {
+    0:'red',
+    1:'blue',
+    2:'green',
+    3:'orange',
+    4:'black',
+    5:'yellow'
+}
 
-# output to static HTML file
-output_file("lines.html")
+def plot_2D_single(sim_obj, valuename):
+    # output to static HTML file
+    output_file(valuename+'_lines.html')
+    bench = []
+    y_plot = {}
+    for sim_name in sim_obj:
+        for bench_name in sim_obj[sim_name]:
+            bench.append(bench_name)
+        break
 
-# create a new plot with a title and axis labels
-p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+    # create a new plot with a title and axis labels
+    #必须要设置x_range,否则当x为字符时输出是一个空图像
+    p = figure(tools="pan,box_zoom,reset,save", 
+        title=valuename,
+        x_range=bench, 
+        x_axis_label='bench_name', y_axis_label='value'
+    )
 
-# add a line renderer with legend and line thickness
-p.line(x, y, legend="Temp.", line_width=2)
-
-# show the results
-show(p)
+    i = 0
+    for sim_name in sim_obj:
+        y = []
+        for bench_name in sim_obj[sim_name]:
+            #print sim_obj[sim_name][bench_name][valuename]
+            y.append(sim_obj[sim_name][bench_name][valuename])
+        #print y
+        y_plot[i] = y
+        p.line(bench, y_plot[i], legend=sim_name, line_width=3, line_color=color_index[i])
+        i += 1
+    # show the results
+    show(p)
