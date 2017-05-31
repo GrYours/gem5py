0bacf699a0d587d84d28ab03aa3de6088f1ba2ee
GrYours <786646652@qq.com>
2017-05-31 03:37:47 -0700

    version 2.2_temp

--- plot_line.py
+++ plot_line.py
@@ -1,10 +1,10 @@
 #-*- coding:utf-8 -*-
 # from bokeh.plotting import figure, output_file, show
 from bokeh.io import curdoc
-from bokeh.layouts import row, widgetbox
+from bokeh.layouts import row, column, widgetbox
 from bokeh.models import ColumnDataSource
-from bokeh.models.widgets import TextInput, Select, PreText, MultiSelect
-from bokeh.plotting import figure
+from bokeh.models.widgets import TextInput, Select, PreText, MultiSelect, Toggle, Button
+from bokeh.plotting import figure, show
 
 import json
 
@@ -50,8 +50,7 @@ bench_name = Select(title="bench_name", value='bcnt', options=bench_name_list)
 value_name = Select(title="value_name", value='sim_seconds', options=value_name_list)
 real_value = PreText(text='real_value: 0')
 samesim_diffbench_average = PreText(text='samesim_diffbench_average: 0')
-# samesim_diffbench_max = PreText(text='samesim_diffbench_max: 0')
-# samesim_diffbench_min = PreText(text='samesim_diffbench_min: 0')
+toggle = Toggle(label='Active!', active=False)
 
 plot0 = figure(tools="pan,box_zoom,reset,save", 
         title=value_name.value,
@@ -81,16 +80,19 @@ def update_stats():
 
 # Set up callbacks
 def update_value_name(attrname, old, new):
-    update_stats()
-    plot0.title.text = value_name.value
-    i = 0
-    for simname in sim_obj:
-        y = []
-        for benchname in sim_obj[simname]:
-            y.append(sim_obj[simname][benchname][value_name.value])
-        y_plot0[i] = y
-        globals()['source0'+str(i)].data = dict(x=bench_name_list, y=y_plot0[i])       # update data
-        i += 1
+    if toggle.active == True:
+        update_stats()
+        plot0.title.text = value_name.value
+        i = 0
+        for simname in sim_obj:
+            y = []
+            for benchname in sim_obj[simname]:
+                y.append(sim_obj[simname][benchname][value_name.value])
+            y_plot0[i] = y
+            globals()['source0'+str(i)].data = dict(x=bench_name_list, y=y_plot0[i])       # update data
+            i += 1
+    else:
+        print 'Waiting!'
 
 for w in [value_name, sim_name, bench_name]:
     w.on_change('value', update_value_name)    
@@ -99,70 +101,65 @@ for w in [value_name, sim_name, bench_name]:
 ##############################################
 value_name_mul = MultiSelect(title="value_name_mul", value=['sim_seconds','sim_ticks'], options=value_name_list)
 
-plot1 = figure(tools="pan,box_zoom,reset,save", 
+# plot1 = figure(tools="pan,box_zoom,reset,save", 
+#         title=sim_name.value,
+#         x_range=value_name_mul.value, 
+#         x_axis_label='value_name', y_axis_label='value'
+#     )
+
+# y_plot1 = {}
+# i = 0
+# for benchname in bench_name_list:
+#     y = []
+#     for valuename in value_name_mul.value:
+#         y.append(sim_obj[sim_name.value][benchname][valuename])
+#     y_plot1[i] = y
+#     globals()['source1'+str(i)] = ColumnDataSource(data=dict(x=value_name_mul.value, y=y_plot1[i]))
+#     plot1.line(value_name_mul.value, y_plot1[i], source=globals()['source1'+str(i)], legend=bench_name_list[i], line_width=3, line_color=color_index[i])
+#     i += 1
+
+# Set up callbacks
+def update_plot1(attrname, old, new):
+    # plot1.title.text = sim_name.value
+
+    # i = 0
+    # for benchname in bench_name_list:
+    #     y = []
+    #     for valuename in value_name_mul.value:
+    #         y.append(sim_obj[sim_name.value][benchname][valuename])
+    #     y_plot1[i] = y
+    #     globals()['source1'+str(i)].data = dict(x=value_name_mul.value, y=y_plot1[i])
+    #     i += 1
+    plot1 = figure(tools="pan,box_zoom,reset,save", 
         title=sim_name.value,
         x_range=value_name_mul.value, 
         x_axis_label='value_name', y_axis_label='value'
     )
-
-y_plot1 = {}
-i = 0
-for benchname in bench_name_list:
-    y = []
-    for valuename in value_name_mul.value:
-        y.append(sim_obj[sim_name.value][benchname][valuename])
-    y_plot1[i] = y
-    globals()['source1'+str(i)] = ColumnDataSource(data=dict(x=value_name_mul.value, y=y_plot1[i]))
-    plot1.line(value_name_mul.value, y_plot1[i], source=globals()['source1'+str(i)], legend=bench_name_list[i], line_width=3, line_color=color_index[i])
-    i += 1
-
-# Set up callbacks
-def update_plot1(attrname, old, new):
-    plot1.title.text = sim_name.value
-    plot1.x_range = value_name_mul.value
+    y_plot1 = {}
     i = 0
     for benchname in bench_name_list:
         y = []
         for valuename in value_name_mul.value:
             y.append(sim_obj[sim_name.value][benchname][valuename])
         y_plot1[i] = y
-        globals()['source1'+str(i)].data = dict(x=value_name_mul.value, y=y_plot1[i])
+        plot1.line(value_name_mul.value, y_plot1[i], legend=bench_name_list[i], line_width=3, line_color=color_index[i])
         i += 1
+
+    curdoc().add_root(plot1)
+    # curdoc().remove_root(plot1, setter=None) # 怎么删，头疼
     
 value_name_mul.on_change('value', update_plot1)
 
 ##############################################
-input_text = widgetbox(sim_name, bench_name, value_name, value_name_mul,
-            real_value, samesim_diffbench_average)
+input_text = widgetbox(sim_name, 
+    bench_name, 
+    value_name, 
+    real_value,
+    value_name_mul, 
+    samesim_diffbench_average, 
+    toggle,
+    width = 600)
 
-curdoc().add_root(row(input_text, plot0, plot1))
+curdoc().add_root(row(input_text, plot0))
 curdoc().title = "Lines"
 
-
-# def plot_2D_single(sim_obj, valuename):
-#     # output to static HTML file
-#     output_file(valuename+'_lines.html')
-#     bench = []
-#     for sim_name in sim_obj:
-#         for bench_name in sim_obj[sim_name]:
-#             bench.append(bench_name)
-#         break
-
-#     # create a new plot with a title and axis labels
-#     #必须要设置x_range,否则当x为字符时输出是一个空图像
-#     p = figure(tools="pan,box_zoom,reset,save", 
-#         title=valuename,
-#         x_range=bench, 
-#         x_axis_label='bench_name', y_axis_label='value'
-#     )
-
-#     i = 0
-#     for sim_name in sim_obj:
-#         y = []
-#         for bench_name in sim_obj[sim_name]:
-#             #print sim_obj[sim_name][bench_name][valuename]
-#             y.append(sim_obj[sim_name][bench_name][valuename])
-#         p.line(bench, y, legend=sim_name, line_width=3, line_color=color_index[i])
-#         i += 1
-#     # show the results
-#     show(p)
