854a41fbcae48c9c758d63625dafb634a0a54964
GrYours <786646652@qq.com>
2017-06-01 00:25:20 -0700

    version 2.2

--- plot_line.py
+++ plot_line.py
@@ -1,14 +1,14 @@
 #-*- coding:utf-8 -*-
-# from bokeh.plotting import figure, output_file, show
 from bokeh.io import curdoc
 from bokeh.layouts import row, column, widgetbox
 from bokeh.models import ColumnDataSource
-from bokeh.models.widgets import TextInput, Select, PreText, MultiSelect, Toggle, Button
-from bokeh.plotting import figure, show
+from bokeh.models.widgets import Select, PreText, MultiSelect, Toggle, Button, DataTable, TableColumn
+from bokeh.plotting import figure
 
 import json
 import pandas as pd
 import numpy as np
+import re
 
 # 个数有限，如果需要更多的线段，需要扩展这个index
 color_index = {
@@ -71,7 +71,7 @@ for simname in sim_obj:
     y_plot0[i] = y
     globals()['source0'+str(i)] = ColumnDataSource(data=dict(x=bench_name_list, y=y_plot0[i]))        # 定义source，这个很重要
     plot0.line(bench_name_list, y_plot0[i], source=globals()['source0'+str(i)], legend=sim_name_list[i], line_width=3, line_color=color_index[i])
-    i += 1
+    i += 1    
 
 # updata value and avg
 def update_stats():
@@ -82,48 +82,23 @@ def update_stats():
     avg = avg / len(bench_name_list)
     samesim_diffbench_average.text = 'samesim_diffbench_average: '+str(avg)
 
-# Set up callbacks
-# def update_value_name(attrname, old, new):
-#     if toggle.active == True:
-#         update_stats()
-#         plot0.title.text = value_name.value
-#         i = 0
-#         for simname in sim_obj:
-#             y = []
-#             for benchname in sim_obj[simname]:
-#                 y.append(sim_obj[simname][benchname][value_name.value])
-#             y_plot0[i] = y
-#             globals()['source0'+str(i)].data = dict(x=bench_name_list, y=y_plot0[i])       # update data
-#             i += 1
-#     else:
-#         print 'Waiting!'
-# for w in [value_name, sim_name, bench_name]:
-#     w.on_change('value', update_value_name)    
 
-##############################################
+table_data = dict(
+        valuename_s=['None']*10,
+        value_s=['None']*10,
+        value_cdf=['None']*10,
+        value_pdf=['None']*10,
+    )
+table_source = ColumnDataSource(table_data)
 
-# Set up callbacks
-# def update_plot1(attrname, old, new):
-#     if toggle.active == True:
-#         plot1 = figure(tools="pan,box_zoom,reset,save", 
-#             title=sim_name.value,
-#             x_range=value_name_mul.value, 
-#             x_axis_label='value_name', y_axis_label='value'
-#         )
-#         y_plot1 = {}
-#         i = 0
-#         for benchname in bench_name_list:
-#             y = []
-#             for valuename in value_name_mul.value:
-#                 y.append(sim_obj[sim_name.value][benchname][valuename])
-#             y_plot1[i] = y
-#             plot1.line(value_name_mul.value, y_plot1[i], legend=bench_name_list[i], line_width=3, line_color=color_index[i])
-#             i += 1
+columns = [
+        TableColumn(field="valuename_s", title="Valuename", width=800),
+        TableColumn(field="value_s", title="Value"),
+        TableColumn(field="value_cdf", title="Value_cdf"),
+        TableColumn(field="value_pdf", title="Value_pdf"),
+    ]
+table = DataTable(source=table_source, columns=columns, width=800)
 
-#         curdoc().add_root(plot1)
-#         # curdoc().remove_root(plot1, setter=None) # 怎么删，头疼
-    
-# value_name_mul.on_change('value', update_plot1)
 
 def update_test():
     update_stats()
@@ -156,6 +131,50 @@ def update_test():
         curdoc().add_root(plot1)
         # curdoc().remove_root(plot1, setter=None) # 怎么删，头疼
 
+    # 截取:号前的部分
+    value_name_temp = ''
+    for str_t in value_name.value:
+        if str_t == ':':
+            break
+        value_name_temp = value_name_temp + str_t
+    # print value_name_temp
+    value_name_table = []
+    value_table = []
+    value_table_cdf = {}
+    value_table_pdf = {}
+    for valuename in value_name_list:
+        if re.match(value_name_temp+'\S+'+'_cdf', valuename):
+            valuename_nocdf = valuename[:-4]
+            # print valuename_nocdf     
+            value_table_cdf[valuename_nocdf] = sim_obj[sim_name.value][bench_name.value][valuename]
+        elif re.match(value_name_temp+'\S+'+'_pdf', valuename):
+            valuename_nopdf = valuename[:-4]
+            # print valuename_nopdf
+            value_table_pdf[valuename_nopdf] = sim_obj[sim_name.value][bench_name.value][valuename]
+        elif re.match(value_name_temp+'\S+', valuename):
+            # print valuename
+            value_name_table.append(valuename)
+            value_table.append(sim_obj[sim_name.value][bench_name.value][valuename])
+
+    value_table_cdf_list = []
+    value_table_pdf_list = []
+    for valuename in value_name_table:
+        if valuename in value_table_cdf:
+            value_table_cdf_list.append(value_table_cdf[valuename])
+        else:
+            value_table_cdf_list.append('None')
+        if valuename in value_table_pdf:
+            value_table_pdf_list.append(value_table_cdf[valuename])
+        else:
+            value_table_pdf_list.append('None')
+    
+    table_source.data = dict(
+        valuename_s=value_name_table,
+        value_s=value_table,
+        value_cdf=value_table_cdf_list,
+        value_pdf=value_table_pdf_list,
+    )
+
 button.on_click(update_test)
 
 ##############################################
@@ -167,25 +186,9 @@ input_text = widgetbox(sim_name,
     samesim_diffbench_average, 
     toggle,
     button,
-    width = 600)
+    table,
+    width = 800)
 
 curdoc().add_root(row(input_text, plot0))
 curdoc().title = "Lines"
 
-
-# plot1 = figure(tools="pan,box_zoom,reset,save", 
-#         title=sim_name.value,
-#         x_range=value_name_mul.value, 
-#         x_axis_label='value_name', y_axis_label='value'
-#     )
-
-# y_plot1 = {}
-# i = 0
-# for benchname in bench_name_list:
-#     y = []
-#     for valuename in value_name_mul.value:
-#         y.append(sim_obj[sim_name.value][benchname][valuename])
-#     y_plot1[i] = y
-#     globals()['source1'+str(i)] = ColumnDataSource(data=dict(x=value_name_mul.value, y=y_plot1[i]))
-#     plot1.line(value_name_mul.value, y_plot1[i], source=globals()['source1'+str(i)], legend=bench_name_list[i], line_width=3, line_color=color_index[i])
-#     i += 1
